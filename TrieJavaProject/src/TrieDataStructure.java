bc5e2fb77aed0536d6023ad5cdaaa58d1c1d205e
d_smorodinnikov <smorodinnikov@mail.ru>
2017-10-13 19:39:25 +0500

    Trie implementation

--- TrieJavaProject/src/TrieDataStructure.java
+++ TrieJavaProject/src/TrieDataStructure.java
@@ -1,18 +1,60 @@
 public class TrieDataStructure implements Trie {
 
+    private TrieNode root;
+
+    public TrieDataStructure() {
+        this.root = new TrieNode();
+    }
 
     @Override
     public void insert(String word) {
-        
+        if (word == null || word.isEmpty())
+            return;
+        TrieNode curr = root;
+        for(int i=0;i<word.length();i++) {
+            TrieNode node = addCh(word.charAt(i), false, curr);
+            curr = node;
+        }
+        curr.setEndOfWord(true);
     }
 
     @Override
     public boolean search(String word) {
-        return false;
+        TrieNode curr = this.root;
+        for(int i=0;i<word.length();i++) {
+            curr = chInTrie(word.charAt(i), curr);
+            if (curr == null)
+                return false;
+        }
+        return curr.isEndOfWord();
     }
 
     @Override
     public boolean startsWith(String prefix) {
-        return false;
+        TrieNode curr = this.root;
+        for(int i=0;i<prefix.length();i++) {
+            curr = chInTrie(prefix.charAt(i), curr);
+            if (curr == null)
+                return false;
+        }
+        return true;
+    }
+
+    public TrieNode getRoot() {
+        return root;
+    }
+
+    private TrieNode chInTrie(char ch, TrieNode curr)
+    {
+        return curr.getChildren().containsKey(ch) ? curr.getChildren().get(ch) : null;
+    }
+
+    private TrieNode addCh(char ch, boolean isLast, TrieNode curr)
+    {
+        if (curr.getChildren().containsKey(ch))
+            return curr.getChildren().get(ch);
+        TrieNode trieNode = new TrieNode(isLast);
+        curr.getChildren().put(ch, trieNode);
+        return trieNode;
     }
 }
---
+++ TrieJavaProject/src/TrieNode.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TrieNode {
+
+    private boolean endOfWord;
+    private Map<Character, TrieNode> children;
+
+    public TrieNode(){
+        children = new HashMap<>();
+    }
+    public TrieNode(boolean isLast){
+        children = new HashMap<>();
+        endOfWord = isLast;
+    }
+
+    public boolean isEndOfWord() {
+        return endOfWord;
+    }
+
+    public void setEndOfWord(boolean endOfWord) {
+        this.endOfWord = endOfWord;
+    }
+
+    public Map<Character, TrieNode> getChildren() {
+        return children;
+    }
+
+    public void setChildren(Map<Character, TrieNode> children) {
+        this.children = children;
+    }
+}
+
+
