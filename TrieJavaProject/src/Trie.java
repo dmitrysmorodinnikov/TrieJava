d9a8793dcec6fe768ab77b91928f350dbd49d5d5
d_smorodinnikov <smorodinnikov@mail.ru>
2017-10-13 17:14:59 +0500

    Trie interface

---
+++ TrieJavaProject/src/Trie.java
@@ -0,0 +1,5 @@
+public interface Trie {
+    void insert(String word);
+    boolean search(String word);
+    boolean startsWith(String prefix);
+}
---
+++ TrieJavaProject/src/TrieDataStructure.java
@@ -0,0 +1,18 @@
+public class TrieDataStructure implements Trie {
+
+
+    @Override
+    public void insert(String word) {
+        
+    }
+
+    @Override
+    public boolean search(String word) {
+        return false;
+    }
+
+    @Override
+    public boolean startsWith(String prefix) {
+        return false;
+    }
+}
