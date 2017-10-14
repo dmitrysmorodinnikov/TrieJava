5ce25fcd74c8b42ed350ec1934198b94537090a7
d_smorodinnikov <smorodinnikov@mail.ru>
2017-10-14 14:14:22 +0500

    Tests
    search tests done

---
+++ TrieJavaProject/tests/search/SearchTest2.java
@@ -0,0 +1,60 @@
+package tests.search;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import tests.TestTries;
+import trie.TrieDataStructure;
+
+public class SearchTest2 {
+
+    private TrieDataStructure ActualTrie;
+
+    @Before
+    public void Init()
+    {
+        ActualTrie = (TrieDataStructure) TestTries.get(1);
+    }
+
+    @Test
+    public void TestMethod1()
+    {
+        Assert.assertFalse(ActualTrie.search("a"));
+    }
+
+    @Test
+    public void TestMethod2()
+    {
+        Assert.assertFalse(ActualTrie.search("ab"));
+    }
+
+    @Test
+    public void TestMethod3()
+    {
+        Assert.assertTrue(ActualTrie.search("abcd"));
+    }
+
+    @Test
+    public void TestMethod4()
+    {
+        Assert.assertFalse(ActualTrie.search("abc"));
+    }
+
+    @Test
+    public void TestMethod5()
+    {
+        Assert.assertFalse(ActualTrie.search("ak"));
+    }
+
+    @Test
+    public void TestMethod6()
+    {
+        Assert.assertFalse(ActualTrie.search("t"));
+    }
+
+    @Test
+    public void TestMethod7()
+    {
+        Assert.assertFalse(ActualTrie.search("12345"));
+    }
+}
---
+++ TrieJavaProject/tests/search/SearchTest3.java
@@ -0,0 +1,78 @@
+package tests.search;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import tests.TestTries;
+import trie.TrieDataStructure;
+
+public class SearchTest3 {
+
+    private TrieDataStructure ActualTrie;
+
+    @Before
+    public void Init()
+    {
+        ActualTrie = (TrieDataStructure) TestTries.get(2);
+    }
+
+    @Test
+    public void TestMethod1()
+    {
+        Assert.assertFalse(ActualTrie.search("a"));
+    }
+
+        @Test
+    public void TestMethod2()
+    {
+        Assert.assertFalse(ActualTrie.search("ab"));
+    }
+
+        @Test
+    public void TestMethod3()
+    {
+        Assert.assertTrue(ActualTrie.search("abcd"));
+    }
+
+        @Test
+    public void TestMethod4()
+    {
+        Assert.assertTrue(ActualTrie.search("abc"));
+    }
+
+        @Test
+    public void TestMethod5()
+    {
+        Assert.assertFalse(ActualTrie.search("ak"));
+    }
+
+        @Test
+    public void TestMethod6()
+    {
+        Assert.assertFalse(ActualTrie.search("t"));
+    }
+
+        @Test
+    public void TestMethod7()
+    {
+        Assert.assertFalse(ActualTrie.search("12345"));
+    }
+
+        @Test
+    public void TestMethod8()
+    {
+        Assert.assertTrue(ActualTrie.search("abgl"));
+    }
+
+        @Test
+    public void TestMethod9()
+    {
+        Assert.assertTrue(ActualTrie.search("cdf"));
+    }
+
+        @Test
+    public void TestMethod10()
+    {
+        Assert.assertTrue(ActualTrie.search("lmn"));
+    }
+}
